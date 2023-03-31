24c5fc9bc3e4d7d346ae9fbf4838e29c704ae4d8
dhkim1206 <69470424+dhkim1206@users.noreply.github.com>
2023-04-01 01:56:31 +0900

    [SoftWare] - 코드 주석 추가

--- FacegoPython/facego.py
+++ FacegoPython/facego.py
@@ -1,16 +1,16 @@
-import cv2
-import mediapipe as mp
-import numpy as np
-from gaze_tracking import GazeTracking
+import cv2 #영상 처리
+import mediapipe as mp # 머신러닝 프레임워크
+import numpy as np # 다차원 배열 처리
+from gaze_tracking import GazeTracking 
 
 # GazeTracking 선언
 gaze = GazeTracking()
 
-# Initialize FaceMesh
+# FaceMesh 초기화
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-# Initialize video capture object
+# 비디오 캡쳐 객체 초기화
 cap = cv2.VideoCapture(0)
 
 # 웹 캠이 켜지지 않았다면
@@ -18,103 +18,99 @@ if not cap.isOpened():
     print("Could not open webcam")
     exit()
 
-# Loop to capture and process each frame of the video
+# 비디오 프레임을 캡처하고 처리하는 루프
 while cap.isOpened():
-    # Capture a frame from the video
+    # 비디오에서 한 프레임을 캡처
     status, frame = cap.read()
 
-    # Check if frame was captured successfully
+    # 프레임 캡처에 실패한 경우
     if not status:
         print("Could not read frame")
         break
 
-    # Flip the frame horizontally for a later selfie-view display
-    # Also convert the color space from BGR to RGB
+    # 프레임을 좌우 대칭으로 바꾸고, BGR에서 RGB로 색상 공간을 변환
     frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
 
-    # To improve performance
+    # 성능을 향상시키기 위해
     frame.flags.writeable = False
 
-    # Get the face landmarks using FaceMesh
+    # FaceMesh를 사용하여 얼굴 랜드마크를 가져옵니다
     results = face_mesh.process(frame)
 
-    # To improve performance
     frame.flags.writeable = True
 
-    # Convert the color space from RGB to BGR
+    # RGB에서 BGR로 색상 공간을 변환합니다
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-    # Get the gaze direction using GazeTracking
+    # 시선 정보 업데이트
     gaze.refresh(frame)
-    frame = gaze.annotated_frame()
+    frame = gaze.annotated_frame() #업데이트된 프레임에 시선 정보 추가하여 보여줌
     text = ""
-    if gaze.is_blinking():
+    if gaze.is_blinking(): # 눈 깜빡일 때
         text = "Eye Blinking"
-    elif gaze.is_right():
+    elif gaze.is_right(): # 오른쪽 볼 때
         text = "Eye Right"
-    elif gaze.is_left():
+    elif gaze.is_left(): # 왼쪽 볼 때 
         text = "Eye left"
-    elif gaze.is_center():
+    elif gaze.is_center(): # 정면 볼 때 
         text = "Eye center"
-    left_pupil = gaze.pupil_left_coords()
-    right_pupil = gaze.pupil_right_coords()
-    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
-    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    left_pupil = gaze.pupil_left_coords() # 왼쪽 눈동자 좌표 가져오기
+    right_pupil = gaze.pupil_right_coords() # 오른쪽 눈동자 좌표 가져오기
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2) # 프레임에 시선 정보 텍스트 추가
+    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1) # 왼쪽 눈동자 좌표
+    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1) # 오른쪽 눈동자 좌표 
 
     # Get the head pose using FaceMesh
-    img_h, img_w, img_c = frame.shape
-    face_3d = []
-    face_2d = []
+    img_h, img_w, img_c = frame.shape # 이미지 높이, 너비, 채널
+    face_3d = [] # 3D 좌표 리스트
+    face_2d = [] # 2D 좌표 리스트
 
-    if results.multi_face_landmarks:
-        for face_landmarks in results.multi_face_landmarks:
-            for idx, lm in enumerate(face_landmarks.landmark):
+    if results.multi_face_landmarks: # 멀티 페이스 랜드마크가 있으면
+        for face_landmarks in results.multi_face_landmarks: #각 페이스 랜드마크에 대해서
+            for idx, lm in enumerate(face_landmarks.landmark): #랜드마크 인덱스와 좌표에 대해서
                 if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
-                    if idx == 1:
-                        nose_2d = (lm.x * img_w, lm.y * img_h)
-                        nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 8000)
+                    if idx == 1: # 코 좌표
+                        nose_2d = (lm.x * img_w, lm.y * img_h) # 코 2D 좌표
+                        nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 8000) # 코 3D 좌표
 
                     x, y = int(lm.x * img_w), int(lm.y * img_h)
 
-                    # Get the 2D Coordinates
+                    # 2D 좌표 가져오기
                     face_2d.append([x, y])
 
-                    # Get the 3D Coordinates
+                    # 3D 좌표 가져오기
                     face_3d.append([x, y, lm.z])
 
-                    # Convert it to the NumPy array
+            # NumPy 배열로 변환
             face_2d = np.array(face_2d, dtype=np.float64)
-
-            # Convert it to the NumPy array
             face_3d = np.array(face_3d, dtype=np.float64)
 
-            # The camera matrix
+            # 카메라 매트릭스
             focal_length = 1 * img_w
 
             cam_matrix = np.array([[focal_length, 0, img_h / 2],
                                    [0, focal_length, img_w / 2],
                                    [0, 0, 1]])
 
-            # The Distance Matrix
+            # 거리 매트릭스
             dist_matrix = np.zeros((4, 1), dtype=np.float64)
 
-            # Solve PnP
+            # PnP 문제 풀기
             success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
 
-            # Get rotational matrix
+            # 회전 행렬 얻기
             rmat, jac = cv2.Rodrigues(rot_vec)
 
-            # Get angles
+            # 각도 가져오기
             angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
 
-            # Get the y rotation degree
+            # Y축 회전 각도 가져오기
             x = angles[0] * 360
             y = angles[1] * 360
 
             # print(y)
 
-            # See where the user's head tilting
+            # 머리 기울기 확인
             if y < -10:
                 text = "Head Left"
             elif y > 10:
@@ -124,19 +120,21 @@ while cap.isOpened():
             else:
                 text = "Head Forward"
 
-            # Display the nose direction
+            # 코 방향 표시
             nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
 
+            # 2D 이미지 상의 코 위치 좌표
             p1 = (int(nose_2d[0]), int(nose_2d[1]))
+            # 3D 공간에서의 코 위치 좌표를 2D이미지상 좌표로 변환하여 좌표 추출
             p2 = (int(nose_3d_projection[0][0][0]), int(nose_3d_projection[0][0][1]))
-
+            # 코 위치 좌표를 이용해 이미지 위에 빨간색 선 그리기
             cv2.line(frame, p1, p2, (255, 0, 0), 2)
 
-            # Add the text on the image
+            # 이미지 위에 텍스트 추가
             cv2.putText(frame, text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
+    # 결과 이미지를 윈도우 창에 표시
     cv2.imshow('Head Pose Estimation', frame)
-
+    # q 눌러서 종료
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
