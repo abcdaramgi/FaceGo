106f8849034618ef4b3b65cb9abfd1c9c76f0244
JIK-K <87060676+JIK-K@users.noreply.github.com>
2023-04-27 19:58:00 +0900

    [Python] client
    0.1 초마다 time 사용하여 데이터 서버로 전송

--- Arduino/socket/client.py
+++ Arduino/socket/client.py
@@ -1,9 +1,16 @@
 import socket
+import threading
+import time
 
+def say_hello():
+    return "hello"
+
+# def test2():
 HOST = '127.0.0.1'
 
-PORT = 9999
-
+PORT = 9999 
+print("클라클라클라라")
+interval = 0.1
 #socket 객체 생성
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,18 +18,23 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
 while True:
-    print("g(직진),r(우회전)은 red b,l은 green 중 입력 (정지하려면 's'입력)")
-    message=input()
+    # print("g(직진),r(우회전)은 red b,l은 green 중 입력 (정지하려면 's'입력)")
+    # 0.1초마다 뭐 보내기
+    message = say_hello() # 함수 호출하여 결과를 변수에 저장
+    print(message) # 결과 출력(확인용)
+    time.sleep(interval) # 일정 시간 동안 대기
 
     #q 입력 시 종료
     if message == 'q':
         break
-    #입력한 message 전송
-    client_socket.sendall(message.encode())
+    if message != None:
 
-    #메시지 수신
-    data = client_socket.recv(1024)
-    print('Received', repr(data.decode()))
+        #입력한 message 전송
+        client_socket.sendall(message.encode())
+
+        #메시지 수신
+        data = client_socket.recv(1024)
+        print('Received', repr(data.decode()))
 
 
 client_socket.close()
